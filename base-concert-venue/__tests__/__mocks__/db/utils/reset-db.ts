14fbf2f451c99450944102594ed8e40b274598cd
Paweł Milewczyk <pawel.milewczyk@jit.team>
2022-08-02 12:14:25 +0200

    create reset-db fn

---
+++ base-concert-venue/__tests__/__mocks__/db/utils/reset-db.ts
@@ -0,0 +1,23 @@
+import { filenames, writeJSONToFile } from "@/lib/db/db-utils";
+
+import { readFakeData } from "../../fakeData";
+
+export const resetDB = async () => {
+  // failsafe against resetting production db!
+  const safeToReset = process.env.NODE_ENV === "test";
+  if (!safeToReset) {
+    // eslint-disable-next-line no-console
+    console.log("WARNING: database reset unavailable outside test environment");
+    return;
+  }
+
+  const { fakeBands, fakeReservations, fakeShows, fakeUsers } =
+    await readFakeData();
+
+  await Promise.all([
+    writeJSONToFile(filenames.bands, fakeBands),
+    writeJSONToFile(filenames.reservations, fakeReservations),
+    writeJSONToFile(filenames.shows, fakeShows),
+    writeJSONToFile(filenames.users, fakeUsers),
+  ]);
+};
