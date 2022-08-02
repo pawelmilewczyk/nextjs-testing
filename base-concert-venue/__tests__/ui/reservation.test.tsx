f9fe2089cc1cfac7054d0204fb0639c3d495f141
Paweł Milewczyk <pawel.milewczyk@jit.team>
2022-08-02 10:52:08 +0200

    add different responses in msw

--- base-concert-venue/__tests__/ui/reservation.test.tsx
+++ base-concert-venue/__tests__/ui/reservation.test.tsx
@@ -2,8 +2,21 @@ import { render, screen } from "@testing-library/react";
 
 import { Reservation } from "@/components/reservations/Reservation";
 
-test("reservation page shows correct number of available seats", async () => {
+test("displays reservations and 'purchase more' button when reservation exists", async () => {
   render(<Reservation showId={0} submitPurchase={jest.fn()} />);
+
   const seatCountText = await screen.findByText(/10 seats left/i);
   expect(seatCountText).toBeInTheDocument();
+});
+
+test("displays no reservations and 'purchase' button when no reservation exists", async () => {
+  render(<Reservation showId={1} submitPurchase={jest.fn()} />);
+
+  const soldOutMessage = await screen.findByRole("heading", {
+    name: /sold out/i,
+  });
+  expect(soldOutMessage).toBeInTheDocument();
+
+  const purchaseButton = screen.queryByRole("button", { name: /purchase/i });
+  expect(purchaseButton).not.toBeInTheDocument();
 });
--- base-concert-venue/__tests__/ui/user-reservations.test.tsx
+++ base-concert-venue/__tests__/ui/user-reservations.test.tsx
@@ -2,11 +2,23 @@ import { render, screen } from "@testing-library/react";
 
 import { UserReservations } from "@/components/user/UserReservations";
 
-test("should display correct purchase button text", async () => {
+test("should display 'purchase more tickets' button", async () => {
   render(<UserReservations userId={1} />);
 
   const button = await screen.findByRole("button", {
     name: /purchase more tickets/i,
   });
   expect(button).toBeInTheDocument();
+});
+
+test('should display "purchase tickets" and should not display "Your tickets" heading if userId === 0', async () => {
+  render(<UserReservations userId={0} />);
+
+  const button = await screen.findByRole("button", {
+    name: /purchase tickets/i,
+  });
+  expect(button).toBeInTheDocument();
+
+  const heading = screen.queryByRole("heading", { name: /your tickets/i });
+  expect(heading).not.toBeInTheDocument();
 });
