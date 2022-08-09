0be785e5143f7f0b9145f322b87b96cb0815234a
Paweł Milewczyk <pawel.milewczyk@jit.team>
2022-08-09 14:55:34 +0200

    add cypress

---
+++ base-concert-venue/cypress/integration/routes.test.js
@@ -0,0 +1,18 @@
+it("displays correct heading when navigating to shows route", () => {
+  cy.visit("/");
+  cy.findByRole("button", { name: /shows/i }).click();
+  cy.findByRole("heading", { name: /upcoming shows/i }).should("exist");
+});
+
+it("displays correct heading when navigating to bands route", () => {
+  cy.visit("/");
+  cy.findByRole("button", { name: /bands/i }).click();
+  cy.findByRole("heading", { name: /our illustrious performers/i }).should(
+    "exist"
+  );
+});
+
+it("displays correct band name for band route that existed at build time", () => {
+  cy.task("db:reset").visit("/bands/94379");
+  cy.findByRole("heading", { name: /Shamrock Pete/i }).should("exist");
+});
